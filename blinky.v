15a05f12233fda4f521929166da3a807519b3f5f
Richard George <rg.george1@gmail.com>
2021-02-11 19:49:11 +0000

    Added comments

--- blinky.v
+++ blinky.v
@@ -9,20 +9,29 @@ module top(input clk_25mhz,
     assign wifi_gpio0 = 1'b1;
     assign i_clk = clk_25mhz;
     reg [7:0] o_led;
-    assign led= o_led;
+    
+    // Assign the LED register to the LED outputs
+    assign led = o_led;
+    
+    // Define a 1 bit register to hold the slow clock
     reg slow_clk;
-
+    
+    // Define two 32 bit counters
     localparam ctr_width = 32;
-    reg [ctr_width-1:0] ctr = 0;
+    reg [ctr_width-1:0] ctr1 = 0;
     reg [ctr_width-1:0] ctr2 = 0;
 
+    // Increment ctr1 on each cycle of the 25MHz clock
+    // Take slow_clk from bit 21 = 25MHz / 2^21 = 12Hz 
     always @(posedge i_clk) begin
-        ctr <= ctr + 1;
-        slow_clk <= ctr[21];
+        ctr1 <= ctr1 + 1;
+        slow_clk <= ctr1[21];
     end
     
+    // On each upwards transition of slow_clk
     always @(posedge slow_clk) begin
     
+        // Reset ctr2 if it has overflowed, otherwise increment ctr2
     	if (ctr2 == 13)
     	begin
     	   ctr2 <= 0;
@@ -32,6 +41,7 @@ module top(input clk_25mhz,
     	   ctr2 <= ctr2 + 1;
     	end
     	
+    	// Choose an output pattern based on the value of ctr2
     	case (ctr2)
     	     0 : o_led[7:0] = 8'b00000001;
     	     1 : o_led[7:0] = 8'b00000010;
